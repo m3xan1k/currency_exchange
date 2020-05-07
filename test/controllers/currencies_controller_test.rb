064755df7273e2673514f1c1d6df4d6b2ae223be
Shevtsov Sergey <dev.serzh@gmail.com>
2020-05-07 13:07:18 +0300

    api tests

--- test/controllers/currencies_controller_test.rb
+++ test/controllers/currencies_controller_test.rb
@@ -1,4 +1,5 @@
 require 'test_helper'
+require 'database_cleaner/active_record'
 
 class CurrenciesControllerTest < ActionDispatch::IntegrationTest
   def setup
@@ -7,8 +8,39 @@ class CurrenciesControllerTest < ActionDispatch::IntegrationTest
     end
   end
 
+  def teardown
+    DatabaseCleaner.clean_with :truncation
+  end
+
   test "must return array of all currencies" do
     get currencies_path
     assert_response :success
+
+    body = ActiveSupport::JSON.decode(response.body)
+    assert body["data"].size == 5
+  end
+
+  test "must return correct currency" do
+    get currencies_path
+    body = ActiveSupport::JSON.decode(response.body)
+    currency = body["data"][0]
+
+    get currency_path(currency["id"])
+    assert_response :success
+
+    body = ActiveSupport::JSON.decode(response.body)
+    currency_detail = body["data"]
+
+    assert currency["name"] == currency_detail["name"]
+    assert currency["rate"] == currency_detail["rate"]
+  end
+
+  test "must return not found" do
+    get currencies_path
+    body = ActiveSupport::JSON.decode(response.body)
+    currency = body["data"][0]
+
+    get currency_path(currency["id"] + 10)
+    assert_response :not_found
   end
 end
