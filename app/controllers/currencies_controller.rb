2342bdb005522bf65747273a78e88a4cc315f50d
Shevtsov Sergey <dev.serzh@gmail.com>
2020-05-07 12:25:59 +0300

    show action

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -4,6 +4,16 @@ class CurrenciesController < ApplicationController
   # GET /currencies
   def index
     @currencies = Currency.all
-    render json: { data: @currencies }
+    render(json: { data: @currencies }, status: 200)
+  end
+
+  # GET /currencies/:id
+  def show
+    @currency = Currency.find_by(id: params[:id])
+    if @currency.nil?
+      render(json: { msg: 'Currency not found' }, status: 404)
+    else
+      render(json: { data: @currency }, status: 200)
+    end
   end
 end
